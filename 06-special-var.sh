ef2f4e3acc8fd6656006468a9d344a0de799b496
shivv <rachamallashivashankargoud0@gmail.com>
2026-01-02 09:22:29 +0530

    script

--- 06-special-var.sh
+++ 06-special-var.sh
@@ -2,3 +2,8 @@
 
 echo "all variables passed to the script: $@"
 echo "all variables passed to the script: $*"
+echo "script name : $01"
+echo "currennt directory : $PWD"
+echo " who is running this : $USER"
+echo "Home directory of user : $HOME"
+echo "PID of this script  : $$"
