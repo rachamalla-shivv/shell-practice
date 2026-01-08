08dd2096d8678636a3291f150e92f347e8cb2f17
shivv <rachamallashivashankargoud0@gmail.com>
2026-01-08 19:54:50 +0530

    colors

--- 13-colors.sh
+++ 13-colors.sh
@@ -22,11 +22,29 @@ VALIDATE() {
     fi
 }
 
-dnf install mysql -y
-VALIDATE $? "MySQL"
+dnf list installed mysql
+# install if it is not found
+if [ $? -ne 0 ]; then
+    dnf install mysql -y
+    VALIDATE $? "MySQL"
+else
+    echo "MySQL already exist ... $Y skipping $N"
+fi
 
-dnf install nginx -y
-VALIDATE $? "NGINX"
+dnf list installed nginx
+# install if it is not found
+if [ $? -ne 0 ]; then
+    dnf install nginx -y
+    VALIDATE $? "NGINX"
+else
+    echo "NGINX already exist...$Y skipping $N"
+fi
 
-dnf install python3 -y
-VALIDATE $? "PYTHON"
+dnf list installed python3
+# install if it is not found
+if [ $? -ne 0 ]; then
+    dnf install python3 -y
+    VALIDATE $? "PYTHON"
+else 
+    echo "PYTHON3 already exists...$Y Skipping $N"
+fi
