0f66c55b887c01fb932384cf46c6d525cdf7a8f1
shivv <rachamallashivashankargoud0@gmail.com>
2026-01-08 11:39:34 +0530

    loop

--- 9-conditions.sh
+++ 9-conditions.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+echo "Please enter the number:"
 read NUMBER
 
-if [ $NUMBER % 2 -eq  0 ]; then
+if [$ (($NUMBER % 2)) -eq  0 ]; then
     echo "$NUMBER is even"
 else
     echo "$NUMBER is ODD"
