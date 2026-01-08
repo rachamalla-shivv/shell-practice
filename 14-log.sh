380daede3e54d8589be71d832d82810052f5ea11
shivv <rachamallashivashankargoud0@gmail.com>
2026-01-08 20:48:25 +0530

    log

--- 14-log.sh
+++ 14-log.sh
@@ -16,9 +16,9 @@ Y="\e[33m"
 N="\e[0m"
 
 
-LOG_FOLDER=/var/log/shell-script
+LOG_FOLDER="/var/log/shell-script"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
-LOGS_FILE=$LOG_FOLDER/$SCRIPT_NAME.log
+LOGS_FILE="$LOG_FOLDER/$SCRIPT_NAME.log"
 
 mkdir -p $LOG_FOLDER
 
